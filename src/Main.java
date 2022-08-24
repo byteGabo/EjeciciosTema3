0e57bb85cb24aa48b8ed6db02c34093dd194eda0
Gabriel Chaves <gabovvv@gmail.com>
2022-08-23 20:13:50 -0600

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(sumas(20,30,10));
+
+        coche micoche = new coche();
+        micoche.setPuertas(2);
+        System.out.println("El coche tiene: " + micoche.getPuertas() + " puertas");
+
+    }
+
+    public static int sumas(int a, int b, int c){
+        return a + b + c;
+    }
+
+    public static class coche{
+        private int puertas = 0;
+
+        public int getPuertas(){
+            return puertas;
+        }
+
+        public void setPuertas(int nuevaPuerta){
+            puertas += nuevaPuerta;
+        }
+    }
+}
